7bc97c62ca471179a03fc198f160beca29967826
saamm <soumyasinha161093@gmail.com>
2026-05-12 03:50:37 +0530

    performing pca analysis on the clusters formed

---
+++ src/pca_analysis_clusters.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+
+# Load data
+X = pd.read_csv("tcga_data/processed/pathway_scores.csv", index_col=0)
+clusters = pd.read_csv("tcga_data/processed/pathway_clusters.csv", index_col=0)
+
+# PCA
+pca = PCA(n_components=2)
+X_pca = pca.fit_transform(X)
+
+# Plot
+plt.figure()
+for c in clusters['cluster'].unique():
+    idx = clusters['cluster'] == c
+    plt.scatter(X_pca[idx, 0], X_pca[idx, 1], label=f"Cluster {c}")
+
+plt.xlabel("PC1")
+plt.ylabel("PC2")
+plt.legend()
+plt.title("PCA of Pathway Activity")
+plt.show()
